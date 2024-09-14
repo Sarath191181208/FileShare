ada37ac39390c109ee306df41e1785b3e429f114
Sarath191181208 <vssarathc04@gmail.com>
2024-09-14 20:12:26 +0530

    +metadata table

---
+++ internal/data/metadata.go
@@ -0,0 +1,23 @@
+package data
+
+import (
+	"database/sql"
+	"time"
+)
+
+type MetaData struct{
+  ID int64 `json:"id"`
+  Name string `json:"name"`
+  UploadDate time.Time `json:"upload_date"`
+  Size int64 `json:"size"`
+  ContentType string `json:"content_type"`
+}
+
+type MetaDataModel struct{
+  DB *sql.DB
+}
+
+func (m *MetaDataModel) Insert(metaData *MetaData) error {
+  stmt := `INSERT INTO metadata (name, upload_date, size, content_type) VALUES ($1, $2, $3, $4) RETURNING id`
+  return m.DB.QueryRow(stmt, metaData.Name, metaData.UploadDate, metaData.Size, metaData.ContentType).Scan(&metaData.ID)
+}
--- internal/data/models.go
+++ internal/data/models.go
@@ -4,10 +4,12 @@ import "database/sql"
 
 type Models struct {
 	Users UserModel 
+  MetaData MetaDataModel
 }
 
 func NewModels(db *sql.DB) *Models {
 	return &Models{
 		Users: UserModel{DB: db}, 
+    MetaData: MetaDataModel{DB: db},
 	}
 }
