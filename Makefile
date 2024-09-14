ff7cb7329764ef9b2523ef41985c8f9f9cddc6cf
Sarath191181208 <vssarathc04@gmail.com>
2024-09-14 14:15:35 +0530

    +db initialized

---
+++ Makefile
@@ -0,0 +1,28 @@
+MAIN_PACKAGE_PATH := ./cmd
+BINARY_NAME := main
+
+# ==================================================================================== #
+# HELPERS
+# ==================================================================================== #
+
+## help: print this help message
+.PHONY: help
+help:
+	@echo 'Usage:'
+	@sed -n 's/^##//p' ${MAKEFILE_LIST} | column -t -s ':' |  sed -e 's/^/ /'
+
+.PHONY: confirm
+confirm:
+	@echo -n 'Are you sure? [y/N] ' && read ans && [ $${ans:-N} = y ]
+
+# ==================================================================================== #
+# DEVELOPMENT
+# ==================================================================================== #
+
+## run/live: run the application with reloading on file changes
+.PHONY: run/live
+run/live:
+	export DB_DSN="postgres://user:psswd@localhost/backend?sslmode=disable" 
+	air \
+		--build.cmd "go build -o /tmp/bin/${BINARY_NAME} ${MAIN_PACKAGE_PATH}" \
+		--build.bin "/tmp/bin/main" \
--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sarath/backend_project/cmd/api"
 	"time"
+
+	_ "github.com/lib/pq"
+
+	"sarath/backend_project/cmd/api"
 )
 
 func main() {
@@ -16,8 +19,8 @@ func main() {
 	// Reading the flags of the application
 	flag.IntVar(&config.Port, "port", 4000, "API server port")
 	flag.StringVar(&config.Env, "env", "dev", "Environment (dev | stag | production)")
-  flag.StringVar(&config.Db.Dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgresSQL DSN")
-  flag.StringVar(&config.Jwt.Secret, "jwt-secret", os.Getenv("jwt-secret"), "The JWT Secret key")
+	flag.StringVar(&config.Db.Dsn, "db-dsn", os.Getenv("DB_DSN"), "PostgresSQL DSN")
+	flag.StringVar(&config.Jwt.Secret, "jwt-secret", os.Getenv("jwt-secret"), "The JWT Secret key")
 	flag.Parse()
 
 	// defining the application
@@ -34,16 +37,36 @@ func main() {
 		WriteTimeout: 10 * time.Minute,
 	}
 
-  // starting the db 
-	//  db, err := OpenDB(app.Config)
-	//  if err != nil{
-	//    app.Logger.Fatal(err)
-	//  }
-	//  defer db.Close()
-	//  app.Logger.Printf("database connection pool established")
-	//
-	// // start the server
-	// app.Logger.Printf("Starting %s server on %s", app.Config.Env, server.Addr)
-  err := server.ListenAndServe()
+	// starting the db
+	db, err := OpenDB(app.Config)
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+	defer db.Close()
+	app.Logger.Printf("database connection pool established")
+
+	// start the server
+	app.Logger.Printf("Starting %s server on %s", app.Config.Env, server.Addr)
+	err := server.ListenAndServe()
 	app.Logger.Fatal(err)
+}
+
+func OpenDB(cfg Config) (*sql.DB, error) {
+	// open an sql connection
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// create a session for the db
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// check for invalid ping
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
