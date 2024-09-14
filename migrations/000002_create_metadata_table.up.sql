0febb723ca7a5261bd20f7835cde742601cbcce4
Sarath191181208 <vssarathc04@gmail.com>
2024-09-14 20:07:45 +0530

    +metadata migrations

---
+++ migrations/000002_create_metadata_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE metadata (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    upload_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    size BIGINT NOT NULL,
+    content_type TEXT NOT NULL
+);
