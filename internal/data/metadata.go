c01a3c393ab9a54b3d73efbbd62c1f88bde93eea
Sarath191181208 <vssarathc04@gmail.com>
2024-09-14 22:37:40 +0530

    +File url and get query

--- internal/data/metadata.go
+++ internal/data/metadata.go
@@ -11,6 +11,7 @@ type MetaData struct{
   UploadDate time.Time `json:"upload_date"`
   Size int64 `json:"size"`
   ContentType string `json:"content_type"`
+  FileUrl string `json:"-"`
 }
 
 type MetaDataModel struct{
@@ -18,6 +19,16 @@ type MetaDataModel struct{
 }
 
 func (m *MetaDataModel) Insert(metaData *MetaData) error {
-  stmt := `INSERT INTO metadata (name, size, content_type) VALUES ($1, $2, $3, $4) RETURNING id`
-  return m.DB.QueryRow(stmt, metaData.Name, metaData.Size, metaData.ContentType).Scan(&metaData.ID)
+  stmt := `INSERT INTO metadata (name, size, content_type, file_url) VALUES ($1, $2, $3, $4) RETURNING id`
+  return m.DB.QueryRow(stmt, metaData.Name, metaData.Size, metaData.ContentType, metaData.FileUrl).Scan(&metaData.ID)
+}
+
+func (m *MetaDataModel) Get(id int64) (*MetaData, error) {
+  stmt := `SELECT id, name, upload_date, size, content_type, file_url FROM metadata WHERE id = $1`
+  meta := &MetaData{}
+  err := m.DB.QueryRow(stmt, id).Scan(&meta.ID, &meta.Name, &meta.UploadDate, &meta.Size, &meta.ContentType, &meta.FileUrl)
+  if err != nil {
+    return nil, err
+  }
+  return meta, nil
 }
--- migrations/000002_create_metadata_table.up.sql
+++ migrations/000002_create_metadata_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE metadata (
     name TEXT NOT NULL,
     upload_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     size BIGINT NOT NULL,
-    content_type TEXT NOT NULL
+    content_type TEXT NOT NULL,
+    file_url TEXT NOT NULL
 );
