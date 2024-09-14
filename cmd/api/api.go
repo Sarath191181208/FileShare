c5fee374a70ff31e991fad5e561109e6514dbcba
Sarath191181208 <vssarathc04@gmail.com>
2024-09-14 15:53:30 +0530

    +db conn in application

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,9 @@
 package api
 
-import "log"
+import (
+	"log"
+	"sarath/backend_project/internal/data"
+)
 
 // Delcaring the version global constant
 const version = "1.0.0"
@@ -18,5 +21,6 @@ type Config struct {
 
 type Application struct {
 	Config Config
-	Logger log.Logger
+	Logger *log.Logger
+  Models *data.Models
 }
